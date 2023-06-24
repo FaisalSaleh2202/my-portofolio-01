cdf52a030443991d2f0475ec1ad30ae0f50af2c8
FaisalSaleh2202 <faisal.saleh127@gmail.com>
2023-06-24 19:12:03 +0700

    add routing remove use client from root layout

--- app/layout.js
+++ app/layout.js
@@ -16,60 +16,10 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
-  const [isOpen, setIsOpen] = useState(false);
   return (
     <html lang='en'>
       <head></head>
-      <body className={inter.className}>
-        <motion.nav
-          variants={navVariants}
-          initial='hidden'
-          whileInView='show'
-          className={`${styles.xPaddings} py-6 relative`}
-        >
-          <div className={`flex justify-between px-4`}>
-            <img
-              src='/letter-f.png'
-              className='sm:w-[64px] sm:h-[64px] h-[46px] w-[46px] '
-              alt='logo'
-            />
-            <MaterialUISwitch />
-            <button onClick={() => setIsOpen(!isOpen)}>
-              {isOpen ? (
-                <CloseOutlinedIcon />
-              ) : (
-                <img
-                  src='./menu.svg'
-                  alt='menu'
-                  className='w-[24px] h-[24px] object-contain'
-                />
-              )}
-            </button>
-          </div>
-        </motion.nav>
-        <AnimatePresence>
-          {isOpen && (
-            <motion.div
-              key='a box'
-              initial={{ opacity: 0, x: 50 }}
-              animate={{ opacity: 1, x: 0 }}
-              //Specified Exit animation
-              exit={{ opacity: 0, x: -50 }}
-              className={` ${styles.xPaddings} absolute z-[999] `}
-            >
-              <div className='bg-black h-screen sm:w-[500%] w-[250%]'>
-                <ul>
-                  <li>Home</li>
-                  <li>About</li>
-                  <li>My Works</li>
-                  <li>Contact</li>
-                </ul>
-              </div>
-            </motion.div>
-          )}
-        </AnimatePresence>
-        {children}
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
